3c4ace9b0e74ab935acf4f5d43ea812f47e74351
Alexis Torres <alexistorres@utexas.edu>
2023-04-03 12:53:16 -0500

    Initial Commit

---
+++ src/Program3.java
@@ -0,0 +1,64 @@
+//Name: (Fill this in)
+//EID: (Fill this in)
+
+
+public class Program3 {
+
+    // DO NOT REMOVE OR MODIFY THESE VARIABLES (calculator and treatment_plan)
+    ImpactCalculator calculator;    // the impact calculator
+    int[] treatment_plan;           // array to store the treatment plan
+
+    public Program3() {
+        this.calculator = null;
+    }
+
+    /*
+     * This method is used in lieu of a required constructor signature to initialize
+     * your Program3. After calling a default (no-parameter) constructor, we
+     * will use this method to initialize your Program3.
+     * 
+     *  DO NOT MODIFY THIS METHOD
+     * 
+     */
+    public void initialize(ImpactCalculator ic) {
+        this.calculator = ic;
+        this.treatment_plan = new int[ic.getNumMedicines()];
+    }
+
+
+    /*
+     * This method computes and returns the total impact of the treatment plan. It should
+     * also fill in the treatment_plan array with the correct values.
+     * 
+     * Each element of the treatment_plan array should contain the number of hours
+     * that medicine i should be administered for. For example, if treatment_plan[2] = 5,
+     * then medicine 2 should be administered for 5 hours.
+     * 
+     */
+
+    public int computeImpact() {
+
+        int totalTime = calculator.getTotalTime();
+        int numMedicines = calculator.getNumMedicines();
+
+        // Put your code here
+
+    }
+
+
+    /*
+     * This method prints the treatment plan.
+     */
+    public void printTreatmentPlan() {
+        System.out.println("Please administer medicines 1 through n for the following amounts of time:\n");
+        int hoursForI = 0;
+        int n = calculator.getNumMedicines();
+        for(int i = 0; i < n; i++){
+            // retrieve the amount of hours for medicine i
+            hoursForI = treatment_plan[i]; // ... fill in here ...
+            System.out.println("Medicine " + i + ": " + hoursForI); 
+        }
+    }
+}
+
+
