d8e0a14437b36c7d21426fb2e459a6e5802f8200
Alexis Torres <alexistorres@utexas.edu>
2023-04-14 11:38:16 -0500

    Version 1

--- ProvidedFiles/Program3.java
+++ ProvidedFiles/Program3.java
@@ -26,6 +26,34 @@ public class Program3 {
         this.treatment_plan = new int[ic.getNumMedicines()];
     }
 
+    // Function to calculate the peak hour of effectiveness for the the medicines
+    private int[] findPeak() {
+        int totalTime = calculator.getTotalTime();
+        int numMedicines = calculator.getNumMedicines();
+        int[] peak_med_hours = new int[numMedicines];
+
+        for (int i = 0; i < numMedicines; i++) {
+            peak_med_hours[i] = -1;
+        }
+
+        for (int i = 0; i < numMedicines; i++) {
+            int prev_value = 0;
+            for (int j = 1; j < totalTime + 1; j++) {
+                int impact = calculator.calculateImpact(i, j);
+                if (prev_value == impact) {
+                    peak_med_hours[i] = j - 1; // If i need the index of the hour to be zero indexed then change to 2
+                    break;
+                }
+                prev_value = impact;
+            }
+            if (peak_med_hours[i] == -1) {
+                peak_med_hours[i] = totalTime;
+            }
+        }
+
+        return peak_med_hours;
+    }
+
     /*
      * This method computes and returns the total impact of the treatment plan. It
      * should
@@ -42,9 +70,46 @@ public class Program3 {
 
         int totalTime = calculator.getTotalTime();
         int numMedicines = calculator.getNumMedicines();
+        int running_impact = -1;
+
+        treatment_plan = new int[numMedicines];
+        int[] opt = new int[totalTime + 1];
+        int[] peak_med_hours = findPeak();
+
+        for (int i = 0; i < numMedicines; i++) {
+            treatment_plan[i] = 0;
+        }
+        for (int i = 0; i < totalTime + 1; i++) {
+            opt[i] = -1;
+        }
+
+        for (int hour = 1; hour < totalTime + 1; hour++) {
+            int max_impact = 0;
+            int med_index = -1;
+
+            for (int medicine = 0; medicine < numMedicines; medicine++) {
+                if (peak_med_hours[medicine] >= hour) { // if the medicine has not been maxed out
+                    int impact_change = calculator.calculateImpact(medicine, hour)
+                            - calculator.calculateImpact(medicine, hour - 1);
+                    if (impact_change > max_impact) {
+                        max_impact = impact_change;
+                        med_index = medicine;
+                    }
+
+                }
+
+            }
+            if (max_impact != 0) { // Only add the medicine if the impact imporves
+                treatment_plan[med_index]++;
+                opt[hour] = med_index;
+                running_impact = running_impact + max_impact;
+            }
+
+        }
 
         // Put your code here
-        return 0;
+
+        return running_impact;
     }
 
     /*
